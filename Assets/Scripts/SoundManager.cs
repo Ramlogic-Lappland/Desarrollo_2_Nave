d63fc94e3fbf4d8179b2a236f0412aed7d5c5fed
Lappland <B4d4ssG04t@gmail.com>
2026-07-12 15:53:28 -0300

    Slider not connected but Menu working

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,8 +4,11 @@ public class SoundManager : MonoBehaviour
 {
     [SerializeField] private SoundLibrary sfxLibrary;
     [SerializeField] private AudioSource sfx2DSource;
+    
     public static SoundManager instance;
-
+    
+    private float _masterVolume = 0.5f;
+    
     private void Awake()
     {
         if (instance != null)
@@ -18,12 +21,12 @@ public class SoundManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
-
+    
     public void PlaySound3D(AudioClip clip, Vector3 position)
     {
         if (clip != null)
         {
-            AudioSource.PlayClipAtPoint(clip, position);
+            AudioSource.PlayClipAtPoint(clip, position, _masterVolume);
         }
     }
 
@@ -38,12 +41,11 @@ public class SoundManager : MonoBehaviour
     
         if (clip != null)
         {
-            sfx2DSource.PlayOneShot(clip);
+            sfx2DSource.PlayOneShot(clip, _masterVolume);
         }
         else
         {
             Debug.LogWarning($"SoundManager: Could not find sound effect named '{soundName}' in the SoundLibrary!");
         }
     }
-
 }
